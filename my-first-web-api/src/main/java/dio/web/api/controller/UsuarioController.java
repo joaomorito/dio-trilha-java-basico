82d3a5d12f8a9ce9ce995c9534139ebc5b70cbcf
joaomorito <joaomorito04@gmail.com>
2024-10-31 13:52:47 -0300

    SpringBoot API

---
+++ my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
@@ -0,0 +1,36 @@
+package dio.web.api.controller;
+
+import dio.web.api.model.Usuario;
+import dio.web.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @PostMapping()
+    public void post(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+    @PutMapping()
+    public void put(@RequestBody Usuario usuario){
+        repository.update(usuario);
+    }
+    @GetMapping()
+    public List<Usuario> getAll(){
+        return repository.listAll();
+    }
+    @GetMapping("/user/{id}")
+    public Usuario getOne(@PathVariable("id") Integer id){
+        return repository.finById(id);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repository.remove(id);
+    }
+}
